49d4722bbcec7337d19ec290ceef621b15c9b45d
Ngo Minh Nhut <minhnhut161098@gmail.com>
2022-10-21 10:46:45 +0700

    add config

--- src/App.js
+++ src/App.js
@@ -1,61 +1,73 @@
 import 'antd/dist/antd.css';
-import CategoryViewer from 'CategoryViewer';
+import CategoryViewer from 'components/CategoryViewer';
 import Spacing from 'components/Spacing';
 import { v4 as uuid } from 'uuid';
 
 const exampleData = {
   name: 'Root Node',
+  type: 'root',
   id: uuid(),
   children: [
     {
       name: 'Node 1',
       id: uuid(),
+      type: 'merchant',
       children: [
         {
           name: 'Node 1-1',
           id: uuid(),
+          type: 'store',
         },
         {
           name: 'Node 1-2',
           id: uuid(),
+          type: 'store',
         },
         {
           name: 'Node 1-3',
           id: uuid(),
+          type: 'store',
         },
       ],
     },
     {
       name: 'Node 2',
       id: uuid(),
+      type: 'merchant',
       children: [
         {
           name: 'Node 2-1',
           id: uuid(),
+          type: 'store',
         },
         {
           name: 'Node 2-2',
           id: uuid(),
+          type: 'store',
         },
         {
           name: 'Node 2-3',
           id: uuid(),
+          type: 'store',
         },
       ],
     },
     {
       name: 'Node 3',
       id: uuid(),
+      type: 'merchant',
       children: [
         {
           name: 'Node 3-1',
           id: uuid(),
+          type: 'store',
         },
       ],
     },
     {
       name: 'Node 4',
       id: uuid(),
+      type: 'merchant',
     },
   ],
 };
